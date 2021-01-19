19c2d43ff8947bbe64faad3145b75b5cde632061
XyZerKunG <64718093+XyZerKunG@users.noreply.github.com>
2021-01-19 10:12:19 +0700

    StaticData Link

---
+++ org/xyzer/servertools/DataStatic.java
@@ -0,0 +1,22 @@
+package org.xyzer.servertools;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DataStatic {
+
+    public static String type;
+    public static String version;
+    public static boolean isset;
+    public static int page = 0;
+
+    public static String ConsoleName;
+    public static String JVM;
+    public static boolean Nogui;
+    public static boolean AutoRun;
+
+    public static final ArrayList<String> spigotserver01 = new ArrayList<>();
+    public static final ArrayList<String> spigotserver02 = new ArrayList<>();
+    public static final HashMap<String, String> spigotmap = new HashMap<>();
+    public static boolean spigotcheck = false;
+}
