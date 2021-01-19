3205c3d917d915debc3e0bea5ac440d9d2aefeff
XyZerKunG <64718093+XyZerKunG@users.noreply.github.com>
2021-01-19 10:13:40 +0700

    StaticServerType

---
+++ org/xyzer/servertools/ServerType.java
@@ -0,0 +1,12 @@
+package org.xyzer.servertools;
+
+import java.util.HashMap;
+
+public class ServerType {
+
+    public static String PAPER = "Paper";
+    public static String SPIGOT = "Spigot";
+    public static String VANILLA = "Vanilla";
+    public static String SNAPSHOT = "Snapshot";
+    public static final HashMap<String, String> datalist = new HashMap<>();
+}
